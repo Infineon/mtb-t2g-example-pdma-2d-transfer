0e3c2907c9b615ffbcf83dc8a88f75abcb781445
Koji Mizumoto <koji.mziumoto@infineon.com>
2025-02-27 19:27:54 +0900

    Upload SHA-1: 73db4238b7c08b2f042e77993f2d37eb20c01a75

--- changelog.md
+++ changelog.md
@@ -2,3 +2,4 @@
 V1.0.0 2023-06-09 Initial version
 V2.0.0 2024-11-15 Supported KIT_T2G_C-2D-6M_LITE, and NEXT_HAL
 V2.1.0 2025-01-28 Added I/D-cache disable function and removed Pin initialization function.
+V3.0.0 2025-02-21 Updated to support ModusToolbox™ software v3.4
--- main.c
+++ main.c
@@ -31,7 +31,7 @@
 #include "cybsp.h"
 #include "cy_pdl.h"
 #include "cy_retarget_io.h"
-
+#include "mtb_hal.h"
 
 /*********************************************************************************************************************/
 /*------------------------------------------------------Macros-------------------------------------------------------*/
@@ -66,6 +66,10 @@ const uint8_t SOURCE_DATA[] =
     'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@',
 };
+
+/* For the Retarget -IO (Debug UART) usage */
+static cy_stc_scb_uart_context_t    UART_context;           /** UART context */
+static mtb_hal_uart_t               UART_hal_obj;           /** Debug UART HAL object  */
 
 /*********************************************************************************************************************/
 /*---------------------------------------------Function Implementations----------------------------------------------*/
@@ -134,6 +138,7 @@ static void handle_PDMA_Interrupt(void)
  */
 int main(void)
 {
+    cy_rslt_t result;
 
     /* Initialize the device and board peripherals */
     if (cybsp_init() != CY_RSLT_SUCCESS)
@@ -147,10 +152,33 @@ int main(void)
     SCB_DisableICache();
     SCB_DisableDCache();
 
-    /* Initialize retarget-io to use the debug UART port */
-    Cy_SCB_UART_Init(UART_HW, &UART_config, NULL);
+    /* Debug UART init */
+    result = (cy_rslt_t)Cy_SCB_UART_Init(UART_HW, &UART_config, &UART_context);
+
+    /* UART init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
     Cy_SCB_UART_Enable(UART_HW);
-    cy_retarget_io_init(UART_HW);
+
+    /* Setup the HAL UART */
+    result = mtb_hal_uart_setup(&UART_hal_obj, &UART_hal_config, &UART_context, NULL);
+
+    /* HAL UART init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    result = cy_retarget_io_init(&UART_hal_obj);
+
+    /* HAL retarget_io init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
 
     /* Transmit header to the terminal */
     /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
